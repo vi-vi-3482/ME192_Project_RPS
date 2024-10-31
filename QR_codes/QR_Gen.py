478ab7deec927da4dbfb0c2ac56225343f394438
ViViViViViViViViViViViVIVi <78184297+ViViViViViViViViViViViVIVi@users.noreply.github.com>
2024-10-31 11:19:07 -0700

    Increased scale of generated QR codes

--- QR_codes/QR_Gen.py
+++ QR_codes/QR_Gen.py
@@ -4,10 +4,10 @@ grid_corners = [(0, 0), (1, 0), (0, 1), (1, 1)]
 
 for i, v in enumerate(grid_corners):
     qrcode = segno.make_qr(str(v))
-    qrcode.save("grid_corners_%d.png" % i)
+    qrcode.save("grid_corners_%d.png" % i, scale=30)
 
 rps = ("rock", "paper", "scissor")
 
 for i, v in enumerate(rps):
     qrcode = segno.make_qr(str(v))
-    qrcode.save("rps_%d.png" % i)
+    qrcode.save("rps_%d.png" % i, scale=30)
