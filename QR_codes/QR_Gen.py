4651dd5fbb165bb716a6a2fc4d8909baa27bd33b
Ty <tylertoad@gmail.com>
2024-11-01 15:35:39 -0700

    Changed rps to rps_blocks and created values for cards

--- QR_codes/QR_Gen.py
+++ QR_codes/QR_Gen.py
@@ -6,8 +6,13 @@ for i, v in enumerate(grid_corners):
     qrcode = segno.make_qr(str(v))
     qrcode.save("grid_corners_%d.png" % i, scale=30)
 
-rps = ("rock", "paper", "scissor")
+rps_blocks = ("rock", "paper", "scissor")
+rps_cards = ("rock_card", "paper_card", "scissor_card")
 
-for i, v in enumerate(rps):
+for i, v in enumerate(rps_blocks):
+    qrcode = segno.make_qr(str(v))
+    qrcode.save("rps_%d.png" % i, scale=30)
+
+for i, v in enumerate(rps_cards):
     qrcode = segno.make_qr(str(v))
     qrcode.save("rps_%d.png" % i, scale=30)
