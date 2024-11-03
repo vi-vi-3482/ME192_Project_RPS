02bf63679e0aba8aa11039172891d03f46d8da95
vi-vi-3482 <78184297+ViViViViViViViViViViViVIVi@users.noreply.github.com>
2024-11-03 10:01:54 -0800

    Made file names match the qr code content.

--- QR_codes/QR_Gen.py
+++ QR_codes/QR_Gen.py
@@ -7,13 +7,13 @@ rps_cards = ("rock_card", "paper_card", "scissor_card")
 
 for i, v in enumerate(grid_corners):
     qrcode = segno.make_qr(str(v))
-    qrcode.save("grid_corners_%d.png" % i, scale=10, dark="darkblue")
+    qrcode.save(f"grid_corners_{v}.png", scale=10, dark="darkblue")
 
 
 for i, v in enumerate(rps_blocks):
     qrcode = segno.make_qr(str(v))
-    qrcode.save("rps_block_%d.png" % i, scale=10, dark="darkred")
+    qrcode.save(f"rps_{v}.png", scale=10, dark="darkred")
 
 for i, v in enumerate(rps_cards):
     qrcode = segno.make_qr(str(v))
-    qrcode.save("rps_card_%d.png" % i, scale=10, dark="darkgreen")
+    qrcode.save(f"rps_card_{v}.png", scale=10, dark="darkgreen")
