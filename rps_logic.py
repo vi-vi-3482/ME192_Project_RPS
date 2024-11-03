dd3f3927ad570a3789497de16d6305cc17f7a33f
vi-vi-3482 <78184297+ViViViViViViViViViViViVIVi@users.noreply.github.com>
2024-11-03 10:17:06 -0800

    Created base logic function to determine winning hand.

--- rps_logic.py
+++ rps_logic.py
@@ -1,5 +1,24 @@
+
+
+def match_case(read_card):
+    to_play = None  # default value in case reading fails, this can be used to raise an error later
+    match read_card:
+        case "rock_card":
+            to_play = "paper_block"
+        case "paper_card":
+            to_play = "scissor_block"
+        case "scissors_card":
+            to_play = "rock_block"
+        case _:
+            print("Invalid input")
+
+    print(f"The read card is {read_card}. \nThe block to play is {to_play}.")
+
+    return to_play
+
+
 def main():
-    print("hello world")
+    match_case("scissors_card")
 
 if __name__ == "__main__":
     main()
