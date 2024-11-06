5351423a81707908d99fcee447299afeebc36631
vi-vi-3482 <haider_al@me.com>
2024-11-06 12:22:35 -0800

    Added functions to get the centre of qr codes and to calculate the perspective transform matrix

--- QR_codes/QR_Read.py
+++ QR_codes/QR_Read.py
@@ -90,7 +90,35 @@ def main():
 
         sorted_qr_codes = sort_codes(decoded, detected, qr_strings)
 
+def centres_of_qr(detected):
+    centres = [cord["cxcy"] for cord in detected]
+    centres = [(int(round(x)), int(round(y))) for x, y in centres]
 
+    centroid_x = sum(x for x, y in centres) / len(centres)
+    centroid_y = sum(y for x, y in centres) / len(centres)
+
+    def angle_from_centroid(point):
+        x, y = point
+        return math.atan2(y - centroid_y, x - centroid_x)
+
+    centres = sorted(centres, key=angle_from_centroid)
+    centres = np.array([centres], dtype=np.float32)
+
+    return centres
+
+def perspective_transform(centres):
+    width, height = 500, 500
+    dst_points = np.array([
+        (0, 0),
+        (width - 1, 0),
+        (width - 1, height - 1),
+        (0, height - 1)
+    ], dtype=np.float32)
+    matrix = cv2.getPerspectiveTransform(centres, dst_points)
+
+    warped_image = cv2.warpPerspective(image, matrix, (width, height))
+
+    return matrix, warped_image
 
 if __name__ == "__main__":
     main()
@@ -108,11 +136,18 @@ def main():
 
         print(decoded)
 
-        time.sleep(1)
 
-    sorted_qr_codes = sort_codes(decoded, detected, qr_strings)
+        sorted_qr_codes = sort_codes(decoded, detected, qr_strings)
 
-    print(sorted_qr_codes)
+        print(sorted_qr_codes)
+
+        plane_edges = centres_of_qr(sorted_qr_codes["grid_corners"][1])
+
+        transform_matrix, transform_image = perspective_transform(plane_edges)
+
+        cv2.imshow("warped", transform_image)
+
+
 
 if __name__ == "__main__":
     main()
