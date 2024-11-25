0563be291ddf77f8ce3c660bc918a26df2e08650
Ty <tylertoad@gmail.com>
2024-11-25 15:30:19 -0800

    Setup calibration function with input from user. Pick and place almost done

--- franka_python.py
+++ franka_python.py
@@ -3,9 +3,9 @@ from panda_py import libfranka
 import logging
 
 # Panda hostname/IP and Desk login information of your robot
-hostname = '192.168.3.100'
-username = 'user'
-password = 'password'
+hostname = '10.31.82.199'
+username = 'student.admin'
+password = 'franka1admin'
 logging.basicConfig(level=logging.INFO)
 
 class RobotControl:
@@ -26,14 +26,84 @@ class RobotControl:
         Touches the franka end factor to the four corners of the workspace. QR codes will be placed at each point
         :return:
         """
+        self.panda.move_to_start()
+        pose = self.panda.get_pose()
+        pose[0, 3] += .3
+        pose[1, 3] -= .3
+        self.panda.move_to_pose(pose)
+        pose[2, 3] -= .47
+        self.panda.move_to_pose(pose)
+        input("press enter to continue")
+
+        self.panda.move_to_start()
+        pose = self.panda.get_pose()
+        pose[0, 3] += .3
+        pose[1, 3] += .3
+        self.panda.move_to_pose(pose)
+        pose[2, 3] -= .47
+        self.panda.move_to_pose(pose)
+        input("press enter to continue")
+
+        self.panda.move_to_start()
+        pose = self.panda.get_pose()
+        pose[0, 3] -= .1
+        pose[1, 3] -= .3
+        self.panda.move_to_pose(pose)
+        pose[2, 3] -= .47
+        self.panda.move_to_pose(pose)
+        input("press enter to continue")
+
+        self.panda.move_to_start()
+        pose = self.panda.get_pose()
+        pose[0, 3] -= .1
+        pose[1, 3] += .3
+        self.panda.move_to_pose(pose)
+        pose[2, 3] -= .47
+        self.panda.move_to_pose(pose)
+        input("press enter to continue")
 
         return
 
     def pick_place(self, x, y):
         """
         Runs the pick and place for taking a corresponding block and moving it to the play area
+        """
+        """
+               A = Max camera range, B = Max ranges of the Franka, x,y = current coord, bx,by = current Franka
+               cx,cy = target in franka coords (set playing coords)
         :param x:
         :param y:
         :return:
         """
+        """
+        bx = x*Bx/Ax
+        by = y*By/Ay
+        
+        return to start()
+        pose = get pose
+        
+        adjust position x,y
+        move to position()
+        adjust position drop z
+        move to position()
+        gripper activate()
+        
+        return to start()
+        pose = get pose
+        
+        **set position every time for target playing location, cx, cy fixed locations
+        
+        adjust position x,y ** by cx, cy
+        move to position()
+        adjust position drop z
+        move to position()
+        gripper move() (release) function
+        
+        return to start()
+        pose = get pose
+        """
         pass
+
+def main():
+    robot = RobotControl()
+    robot.calibrate()
