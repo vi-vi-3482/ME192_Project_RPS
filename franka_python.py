dbaa9b5091d09ae35c7673da6dddff6907298b1b
Ty <tylertoad@gmail.com>
2024-11-26 11:17:00 -0800

    Modified move speed of robot, fixed the 4th calibration point movement, modified back 2 calibration points to x=-.1 instead of -.3, also modified coord mapping to account for the off-centered origin due to the x=-.1 change

--- franka_python.py
+++ franka_python.py
@@ -30,37 +30,37 @@ class RobotControl:
         self.panda.move_to_start()
         pose = self.panda.get_pose()
         pose[0, 3] += .3
-        pose[1, 3] -= .3
-        self.panda.move_to_pose(pose)
-        pose[2, 3] -= .47
-        self.panda.move_to_pose(pose)
+        pose[1, 3] -= .2
+        self.panda.move_to_pose(pose,speed_factor=0.07)
+        pose[2, 3] -= .46
+        self.panda.move_to_pose(pose,speed_factor=0.07)
         input("press enter to continue")
 
         self.panda.move_to_start()
         pose = self.panda.get_pose()
         pose[0, 3] += .3
-        pose[1, 3] += .3
-        self.panda.move_to_pose(pose)
-        pose[2, 3] -= .47
-        self.panda.move_to_pose(pose)
+        pose[1, 3] += .2
+        self.panda.move_to_pose(pose,speed_factor=0.07)
+        pose[2, 3] -= .46
+        self.panda.move_to_pose(pose,speed_factor=0.07)
         input("press enter to continue")
 
         self.panda.move_to_start()
         pose = self.panda.get_pose()
-        pose[0, 3] -= .3
-        pose[1, 3] -= .3
-        self.panda.move_to_pose(pose)
-        pose[2, 3] -= .47
-        self.panda.move_to_pose(pose)
+        pose[0, 3] -= .1
+        pose[1, 3] -= .2
+        self.panda.move_to_pose(pose,speed_factor=0.07)
+        pose[2, 3] -= .46
+        self.panda.move_to_pose(pose,speed_factor=0.07)
         input("press enter to continue")
 
         self.panda.move_to_start()
         pose = self.panda.get_pose()
-        pose[0, 3] -= .3
-        pose[1, 3] += .3
-        self.panda.move_to_pose(pose)
-        pose[2, 3] -= .47
-        self.panda.move_to_pose(pose)
+        pose[0, 3] -= .1
+        pose[1, 3] += .2
+        self.panda.move_to_pose(pose,speed_factor=0.07)
+        pose[2, 3] -= .46
+        self.panda.move_to_pose(pose,speed_factor=0.07)
         input("press enter to continue")
 
         return
@@ -79,18 +79,20 @@ class RobotControl:
 
         # bx = x * Bx / Ax
         # by = y * By / Ay
-        franka_x = ((x - 500) / 500) * .3
-        franka_y = ((y - 500) / 500) * .3
+        #set up new x Range to -.1 to +.3 (subtract .1 from ratio value to shift centered coords)
+        #set up new y Rance to -.2 to -.2
+        franka_x = (((x - 500) / 500) * .2)+.1
+        franka_y = ((y - 500) / 500) * .2
         # First, move and pick up
         self.panda.move_to_start()
         pose = self.panda.get_pose()
-        self.panda.move_to_pose(pose)
+
         # Do the math to convert to franka x and y
         pose[0,3] += franka_x  # front/back
         pose[1,3] -= franka_y #left/right
-        self.panda.move_to_pose(pose)
+        self.panda.move_to_pose(pose,speed_factor=0.07)
         pose[2,3] -= .46 #up/down
-        self.panda.move_to_pose(pose)
+        self.panda.move_to_pose(pose,speed_factor=0.07)
         self.gripper.grasp(0, 0.2, 10, 0.04, 0.04)
 
         # Now move and place. Need Permanent Target Coordinates
@@ -98,9 +100,9 @@ class RobotControl:
         pose = self.panda.get_pose()
         # pose[0, 3] += x  # front/back
         # pose[1, 3] -= y  # left/right
-        self.panda.move_to_pose(pose)
+        self.panda.move_to_pose(pose,speed_factor=0.07)
         pose[2, 3] -= .46  # up/down
-        self.panda.move_to_pose(pose)
+        self.panda.move_to_pose(pose,speed_factor=0.07)
         self.gripper.move(0.08, 0.2)
 
         self.panda.move_to_start()
