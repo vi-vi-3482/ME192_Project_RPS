df5a027fd0aece08d978ac83210ef30a50685b72
vi-vi-3482 <haider_al@me.com>
2024-12-02 15:53:08 -0800

    changed match case to if-else for python 3.8 compatability

--- rps_logic.py
+++ rps_logic.py
@@ -1,21 +1,34 @@
 
+# def match_card(read_card):
+#     to_play = None  # default value in case reading fails, this can be used to raise an error later
+#     match read_card:
+#         case "rock_card":
+#             to_play = "paper_block"
+#         case "paper_card":
+#             to_play = "scissor_block"
+#         case "scissors_card":
+#             to_play = "rock_block"
+#         case _:
+#             print("Invalid input")
+#
+#     print(f"The read card is {read_card}. \nThe block to play is {to_play}.")
+#
+#     return to_play
 
 def match_card(read_card):
     to_play = None  # default value in case reading fails, this can be used to raise an error later
-    match read_card:
-        case "rock_card":
-            to_play = "paper_block"
-        case "paper_card":
-            to_play = "scissor_block"
-        case "scissors_card":
-            to_play = "rock_block"
-        case _:
-            print("Invalid input")
+    if read_card == "rock_card":
+        to_play = "paper_block"
+    elif read_card == "paper_card":
+        to_play = "scissor_block"
+    elif read_card == "scissors_card":
+        to_play = "rock_block"
+    else:
+        print("Invalid input")
 
     print(f"The read card is {read_card}. \nThe block to play is {to_play}.")
 
     return to_play
-
 
 def main():
     match_card("scissors_card")
